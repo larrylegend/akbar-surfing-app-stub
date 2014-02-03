5e55167583eb1973fca7780a779686346bec60e3
Larry Legend <larry@houseoflegend.com>
2014-02-02 21:10:44 -0500

    Add stub JSON output for surfing app.

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 
 before do
  	content_type 'application/json'
@@ -8,6 +9,28 @@ get '/' do
  	'{"ping": "pong"}'
 end
 
-get '/status.json' do
-	'{"on_air": true}'
+get '/locations' do
+	content_type :json
+	
+	object = 
+	[
+		{ 	:name => 'Rockaway Beach',
+			:temperature => 71.0,
+			:conditions => 'Calm'
+		},
+		{ 	:name => 'Fire Island',
+			:temperature => 40.0,
+			:conditions => 'Choppy, hailing'
+		},
+		{ 	:name => 'Montauk',
+			:temperature => 66.0,
+			:conditions => 'Eight foot waves'
+		}
+	]
+
+	# compact JSON:
+	# object.to_json
+
+	# pretty JSON:
+	JSON.pretty_generate(object)
 end
